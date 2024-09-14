dd397563a0d3fbece918a1b76c9c2fdc2058ea61
J_SGG <alekseinavalforever@gmail.com>
2024-09-13 23:26:49 -0500

    cambios prisma y metodo post

--- src/components/organization/controller.ts
+++ src/components/organization/controller.ts
@@ -1,49 +1,19 @@
 import { Request, Response } from "express";
 import { PrismaClient } from '@prisma/client'
+import { getAllOrganizations as fetchAllOrganizations, createOrganization as addNewOrganization  } from '../../services/services';
 const prisma = new PrismaClient();
 
 export const createOrganization = async (req: Request, res: Response) => {
     try {
-        const { nameOrganization, ruc, phone, email, address, coordinates, representative, purpose, dependentsBenefit, motive, numPreRegister } = req.body;
+        const data = req.body;
+        const newOrganization = await addNewOrganization(data);
 
-        const newOrganization = await prisma.organization.create({
-            data: {
-                nameOrganization,
-                ruc,
-                phone,
-                email,
-                purpose,
-                dependentsBenefit,
-                motive,
-                numPreRegister,
-                address: {
-                    create: {
-                        street: address.street,
-                        city: address.city,
-                        neighborhood: address.neighborhood,
-                        province: address.province,
-                        country: address.country,
-                    }
-                },
-                coordinates: {
-                    create: {
-                        latitude: coordinates.latitude,
-                        longitude: coordinates.longitude,
-                    }
-                },
-                representative: {
-                    create: {
-                        name: representative.name,
-                        numDoc: representative.numDoc,
-                        role: representative.role,
-                        email: representative.email,
-                        phone: representative.phone,
-                    }
-                }
-            }
-        });
-
-        res.status(201).json(newOrganization);
+        res.statusCode = 201;
+        res.end(JSON.stringify({
+          status: 201,
+          message: 'Organizacion creada correctamente',
+          response: newOrganization
+        }));
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Error al crear la organización' });
@@ -52,17 +22,16 @@ export const createOrganization = async (req: Request, res: Response) => {
 
 export const getAllOrganizations = async (req: Request, res: Response) => {
     try {
-        const organizations = await prisma.organization.findMany({
-            include: {
-                address: true,
-                coordinates: true,
-                representative: true
-            }
-        });
-        res.json(organizations);
+        const organizations = await fetchAllOrganizations();
+        res.statusCode = 200;
+        res.end(JSON.stringify({
+          status: 200,
+          message: 'Todas las Organizaciones obtenidas correctamente',
+          response: organizations,
+        }));
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error al obtener las organizaciones' });
+        res.status(500).json({ message: 'Error al obtener las Organizaciones' });
     }
 };
 
@@ -72,7 +41,7 @@ export const getOrganizationById = async (req: Request, res: Response) => {
         const organization = await prisma.organization.findUnique({
             where: { id: Number(id) },
             select: {
-                stateRegister: true,
+                stateRegistration: true,
                 address: {
                     select: {
                         city: true,
